551d3aeff23f28aacce6e69f1a7783db77f39b89
黃昱謙 <A112223011@mail.shu.edu.tw>
2024-10-23 23:02:54 +0800

    113-10-23

--- HW1/src/Time1.java
+++ HW1/src/Time1.java
@@ -1,15 +1,96 @@
 public class Time1 {
-    //時(hour)
-    //分(minute)
-    //秒(second)
+    private int hour;//時(hour)
+    private int minute;//分(minute)
+    private int second;//秒(second)
 
     //建構子(設定時、分、秒的初始直，需檢查資料是否格式正確)
+    public Time1(int hour, int minute, int second) {
+        if (hour >= 0 && hour < 24) {
+            this.hour = hour;
+        } else {
+            this.hour = 0;
+        }
+
+        if (minute >= 0 && minute < 60) {
+            this.minute = minute;
+        } else {
+            this.minute = 0;
+        }
+
+        if (second >= 0 && second < 60) {
+            this.second = second;
+        } else {
+            this.second = 0;
+        }
+    }
 
     //方法: 是需要加入get/set
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setHour(int hour) {
+        if (hour >= 0 && hour < 24) {
+            this.hour = hour;
+        }
+    }
+
+    public void setMinute(int minute) {
+        if (minute >= 0 && minute < 60) {
+            this.minute = minute;
+        }
+    }
+
+    public void setSecond(int second) {
+        if (second >= 0 && second < 60) {
+            this.second = second;
+        }
+    }
+
 
     //方法: tick (時間增加一秒)
+    public void tick() {
+        second++;
+        if (second == 60) {
+            second = 0;
+            minute++;
+            if (minute == 60) {
+                minute = 0;
+                hour++;
+                if (hour == 24) {
+                    hour = 0;
+                }
+            }
+        }
+    }
 
     //方法: printUniversal (以24小時制輸出完整時間  14:05:32)
+    public void printUniversal() {
+        System.out.printf("%02d:%02d:%02d%n", hour, minute, second);
+    }
 
     //方法: printStandard (以12小時制輸出完整時間  2:05:32  PM)
+    public void printStandard() {
+        int standardHour = (hour == 0 || hour == 12) ? 12 : hour % 12;
+        String amPm = (hour < 12) ? "AM" : "PM";
+        System.out.printf("%02d:%02d:%02d %s%n", standardHour, minute, second, amPm);
+    }
+
+    public static void main(String[] args) {
+        Time1 time = new Time1(13, 5, 32);
+        time.printUniversal();
+        time.printStandard();
+
+        time.tick();
+        time.printUniversal();
+        time.printStandard();
+    }
 }
--- HW1/src/Time1Test.java
+++ HW1/src/Time1Test.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
+
 public class Time1Test {
     public  static  void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
         //Time1 time = new ....
+        Time1 time = new Time1(13, 5, 32);
 
         //呼叫printUniversal()
+        System.out.println("Initial Time (24-hour format): ");
+        time.printUniversal();
 
         //呼叫printStandard()
+        System.out.println("Initial Time (12-hour format): ");
+        time.printStandard();
 
         //使用者輸入時間過了幾秒，用迴圈呼叫tick
+        System.out.print("Enter how many seconds have passed: ");
+        int secondsPassed = scanner.nextInt();
+        for (int i = 0; i < secondsPassed; i++) {
+            time.tick();
+        }
 
         //呼叫printUniversal()
+        System.out.println("Updated Time (24-hour format): ");
+        time.printUniversal();
 
         //呼叫printStandard()
+        System.out.println("Updated Time (12-hour format): ");
+        time.printStandard();
     }
 
 }
