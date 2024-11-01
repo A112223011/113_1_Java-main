cb39927989da46faf93e45cf5186a6edae1e6bdc
A112223011 <A112223011@mail.shu.edu.tw>
2024-11-01 10:55:20 +0800

    113-11-01

--- 113-10-25/StaticMember/src/edu/shu/Joe/CashCard.java
+++ 113-10-25/StaticMember/src/edu/shu/Joe/CashCard.java
@@ -8,7 +8,7 @@ public class CashCard {
     private static int cardOrder;
     //方法的身分辨識 = 方法名稱 + 參數個數 + 參數型態
     public CashCard() {
-        this.number = "unknown";
+        setNumber("unknown");
     }
     public CashCard(String number) {
         setNumber(number);
@@ -18,6 +18,7 @@ public class CashCard {
     public CashCard(String number, int balance) {
         setNumber(number);
         setBalance(balance);
+        System.out.printf("第 %d 張卡%n", ++this.cardOrder);
     }
 
     public CashCard(String number, int balance, int bonus) {
