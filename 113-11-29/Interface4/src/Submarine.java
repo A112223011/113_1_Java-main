a2453f31dc82b27d5356ff942e3d65f6ed98a4ac
A112223011 <A112223011@mail.shu.edu.tw>
2024-11-29 10:48:06 +0800

    113-11-29

---
+++ 113-11-29/Interface4/src/Submarine.java
@@ -0,0 +1,14 @@
+public class Submarine extends Boat implements Diver {
+    public Submarine(String name) {
+        super(name);
+    }
+
+    @Override
+    public void swim() {
+        System.out.printf("潛水艇 %s 航行%n", name);
+    }
+    @Override
+    public void dive() {
+        System.out.printf("潛水艇 %s 潛行%n", name);
+    }
+}      
