8f44c001da55b4169e40f4f13dbe1ab141540cf9
A112223011 <A112223011@mail.shu.edu.tw>
2024-12-13 09:52:55 +0800

    1206

---
+++ 113-12-06/TryCatch/src/MutiExcepton.java
@@ -0,0 +1,10 @@
+public class MutiExcepton {
+    public static void main(String[] args) {
+        int[]numbers = {1, 2, 3,};
+        try {
+            System.out.println(numbers[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("處理未減例外 : "+ e.getMessage());
+        }
+    }
+}
